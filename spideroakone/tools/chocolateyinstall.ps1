23e6e9eaaccbe66f02507b34ad117bbd5f38e911
Martin F. Schumann <mfs@mfs.name>
2018-05-13 21:31:44 +0200

    upgrade spideroakone to version 7.1.0

--- spideroakone/tools/chocolateyinstall.ps1
+++ spideroakone/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'spideroakone'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://spideroak.com/share/M5XWYZDFNY/retriever/f%3A/Fetch/7.0.1/SpiderOakONE-7.0.1-msi_x86.msi'
-$url64      = 'https://spideroak.com/share/M5XWYZDFNY/retriever/f%3A/Fetch/7.0.1/SpiderOakONE-7.0.1-msi_x64.msi'
+$url        = 'https://spideroak.com/share/M5XWYZDFNY/retriever/f%3A/Fetch/7.1.0/SpiderOakONE-7.1.0-msi_x86.msi'
+$url64      = 'https://spideroak.com/share/M5XWYZDFNY/retriever/f%3A/Fetch/7.1.0/SpiderOakONE-7.1.0-msi_x64.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'SpiderOakONE*'
 
-  checksum      = 'f1fef80d3b7d0e18d36f3a33b53954999de45f1604ceeac78c57a5d177338181'
+  checksum      = 'cb795c2d56ba90cd1c71c8a8e54f0b99d324f24b334fdf56c40b8cd94e3f2b78'
   checksumType  = 'sha256'
-  checksum64    = 'f38021df59c14772331c91a1aa6f1b1fdd82e742163158870b8600347608863e'
+  checksum64    = '197f9069bc117f9dfd1416eb9b28170a0e78dfc2955aea31ba520ef98ab9b6e1'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
